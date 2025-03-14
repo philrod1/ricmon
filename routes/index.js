08365073d024d8329782e92cb18076b9b8e6356d
Phil Rodgers <phil@rodgers.it>
2025-03-14 15:41:17 +0000

    Adding appstore endpoints

--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,128 @@ router.get('/', (req, res, next) => {
   res.render('index', {title: 'RIC Stuff'});
 });
 
+router.get('/status', async (req, res, next) => {
+  let isAlive = false;
+  let isReady = false;
+  let dmsReady = false;
+  let e2Status = 0;
+
+  try {
+
+    try {
+      let response = await axios.get(`http://${appMgrIP}:8080/ric/v1/health/alive`);
+      isAlive = response.status === 200;
+    } catch (error) {
+      console.error('Error fetching alive status:');
+    }
+
+    try {
+      response = await axios.get(`http://${appMgrIP}:8080/ric/v1/health/ready`);
+      isReady = response.status === 200;
+    } catch (error) {
+      console.error('Error fetching ready status:');
+    }
+
+    const { stdout: dmsStdout } = await execPromise("/home/evo/.local/bin/dms_cli health");
+
+    dmsReady = dmsStdout.trim() === "True";
+
+    // Fetch E2 status
+    try {
+      let response = await axios.get(`http://${e2MgrIp}:3800/v1/nodeb/states`);
+      if (response.status === 200) {
+        e2Status = response.data[0].connectionStatus === 'CONNECTED' ? 2 : 1;
+      }
+    } catch (error) {
+      console.error('Error fetching E2 status:');
+    }
+
+    console.log(isAlive, isReady, dmsReady, e2Status);
+    res.status(200).send({ isAlive, isReady, dmsReady, e2Status });
+  } catch (error) {
+    console.error('Error executing command:');
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+router.get('/xapp/:name/:version', async (req, res, next) => {
+  const status = {
+    onboarded: false,
+    installed: false,
+    status: 'unknown',
+    ready: false,
+    deployment: null
+  };
+  const { name, version } = req.params;
+  try {
+    const chartJSON = await getJSON('http://127.0.0.1:8090/api/charts', '', 'get');
+    let chartApps;
+    try {
+      chartApps = JSON.parse(chartJSON);
+    } catch (parseError) {
+      console.error('Error parsing chart JSON:', parseError);
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+
+    if (typeof chartApps !== 'object' || chartApps === null) {
+      console.error('chartApps is not an object:', chartApps);
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+
+    const chartAppVersions = chartApps[name];
+    if (!Array.isArray(chartAppVersions)) {
+      console.error(`No versions found for chart ${name}:`, chartApps);
+      res.status(404).send(status);
+      return;
+    }
+
+    const helmApp = chartAppVersions.find(v => v.version === version);
+    
+    if (!helmApp) {
+      res.status(404).send(status);
+      return;
+    }
+    status.onboarded = true;
+
+    const { stdout } = await execPromise(`kubectl get pods -n ricxapp -o json`);
+    const data = JSON.parse(stdout);
+    
+    const item = data.items.find(item => item.metadata.labels.release === name);
+    
+    if (!item) {
+      res.status(200).send(status);
+      return;
+    }
+
+    const { stdout: helmStdout } = await execPromise(`helm list -n ricxapp -o json | jq '.[] | select(.chart == "${name}-${version}")'`);
+    
+    const helmStatus = JSON.parse(helmStdout);
+    
+    if (!helmStatus) {
+      res.status(200).send(status);
+      return;
+    }
+    status.installed = true;
+    status.status = helmStatus.status;
+    status.deployment = item.metadata.name;
+    const containerStatuses = item.status.containerStatuses || [];
+    const containerStatus = containerStatuses.find(status => status.name === name && status.started);
+    
+    if (!containerStatus) {
+      res.status(200).send(status);
+      return;
+    }
+    status.started = containerStatus.started;
+    status.ready = containerStatus.ready;
+    res.status(200).send(status);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
 router.get('/charts', (req, res, next) => {
   // const result = getJSON("http://10.97.12.62:32080/onboard/api/v1/charts", '', 'get');
   const result = getJSON('http://127.0.0.1:8090/api/charts', '', 'get');
